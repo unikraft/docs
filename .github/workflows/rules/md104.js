e167b69ddf96641f98f0999edb2f0cccf8bc7843
Stefan Jumarea <stefanjumarea02@gmail.com>
2022-12-19 11:04:58 +0200

    github/linter: Ignore md104 rule when inside shortcodes
    When using shortcodes, we should ignore the "one line per sentence and
    one sentence per line" linter rule.
    
    Signed-off-by: Stefan Jumarea <stefanjumarea02@gmail.com>

--- .github/workflows/rules/md104.js
+++ .github/workflows/rules/md104.js
@@ -9,13 +9,16 @@ module.exports = {
       return token.type === "inline";
     })) {
       var actual_lines = inline.content.split("\n");
+      var hugo_shortcode = false;
       actual_lines.forEach((line, index, arr) => {
+		if (line.startsWith("{{"))
+		      hugo_shortcode = true;
 		let outside = true;
 		let outside_ticks = true;
 		let count = 0;
 		Array.from(line).forEach((char) => {
 			if ((char == "." || char == "?" || char == "!") &&
-				(outside && outside_ticks)) {
+				(outside && outside_ticks && !hugo_shortcode)) {
 				count++;
 			}
 			if (char == "`") outside_ticks = !outside_ticks;
@@ -24,6 +27,8 @@ module.exports = {
 			if (char == "]") outside = true;
 			if (char == ")") outside = true;
 		});
+		if (line.endsWith("}}"))
+		      hugo_shortcode = false;
         if (count > 1) {
           onError({
             lineNumber: inline.lineNumber + index,
