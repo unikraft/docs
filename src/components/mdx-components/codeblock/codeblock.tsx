3c28d361d8699460a18324db26bae246c93bc86d
Alexander Jung <alex@unikraft.io>
2024-01-08 09:50:53 +0100

    refactor(codeblock): Remove React Live components
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- src/components/mdx-components/codeblock/codeblock.tsx
+++ src/components/mdx-components/codeblock/codeblock.tsx
@@ -1,61 +1,21 @@
-import { Box, Icon, useBoolean } from '@chakra-ui/react'
-import dynamic from 'next/dynamic'
-import React, { useEffect } from 'react'
+import { Box, Icon } from '@chakra-ui/react'
 import CodeContainer from './code-container'
 import CopyButton from './copy-button'
 import Highlight from './highlight'
 import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa'
 
-const ReactLiveBlock = dynamic(() => import('./react-live-block'))
-
 function CodeBlock(props) {
-  const [isMounted, { on }] = useBoolean()
-  useEffect(
-    /**
-     * Lazily-load <ReactLiveBlock /> to save bundle size.
-     */
-    on,
-    [on],
-  )
   const {
     className,
-    live = true,
-    manual,
-    render,
     children,
     viewlines,
     nocopy,
     variant = 'default',
     ln,
-    mountStylesheet = false,
   } = props.children.props
-
-  const _live = live === 'true' || live === true
 
   const language = className?.replace(/language-/, '')
   const rawCode = children.trim()
-
-  const reactLiveBlockProps = {
-    rawCode,
-    language,
-    noInline: manual,
-    mountStylesheet,
-  }
-
-  if (isMounted && language === 'jsx' && _live === true) {
-    return <ReactLiveBlock editable {...reactLiveBlockProps} />
-  }
-
-  if (isMounted && render) {
-    /**
-     * @TODO Not sure if this is even used?
-     */
-    return (
-      <div style={{ marginTop: 32 }}>
-        <ReactLiveBlock editable={false} {...reactLiveBlockProps} />
-      </div>
-    )
-  }
 
   return (
     <Box id='codeblock' className={`codeblock-${variant}`} position='relative' zIndex='0'>
