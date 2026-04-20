28e479018f6f7e215aeeae8019d90a2c0b009998
Razvan Deaconescu <razvand@unikraft.io>
2026-04-20 23:18:41 +0300

    release: Add blog post and notes for release 0.21.0
    Add blog post, notes and support files for release 0.21.0 (Ijiraq).
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- src/components/landing-hero.tsx
+++ src/components/landing-hero.tsx
@@ -18,8 +18,8 @@ import CopyButton from 'components/mdx-components/codeblock/copy-button'
 import { t } from 'utils/i18n'
 
 const announce = {
-  title: 'Unikraft releases 0.20.0',
-  href: '/blog/2025-09-08-unikraft-releases-v0.20.0',
+  title: 'Unikraft releases 0.21.0',
+  href: '/blog/2026-04-20-unikraft-releases-v0.21.0',
 }
 
 export default function LandingHero() {
@@ -245,11 +245,11 @@ export default function LandingHero() {
               }}
             >
               <Stat>
-                <StatNumber>v0.20.0</StatNumber>
+                <StatNumber>v0.21.0</StatNumber>
                 <StatLabel>Latest Version</StatLabel>
               </Stat>
               <Stat>
-                <StatNumber>3.2K+</StatNumber>
+                <StatNumber>3.5K+</StatNumber>
                 <StatLabel>GitHub Stars</StatLabel>
               </Stat>
               <Stat>
@@ -257,7 +257,7 @@ export default function LandingHero() {
                 <StatLabel>Contributors</StatLabel>
               </Stat>
               <Stat>
-                <StatNumber>1.9K+</StatNumber>
+                <StatNumber>2.2K+</StatNumber>
                 <StatLabel>Discord Members</StatLabel>
               </Stat>
             </StatGroup>
